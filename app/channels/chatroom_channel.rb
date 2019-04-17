cc79a2fbca40446d5cff7f1c144a9d8c7f46362d
Marc <akbar13@live.com>
2019-04-17 16:02:47 -0600

    Setup ActionCable

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -1,6 +1,7 @@
 class ChatroomChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    # Requests and subscribes to the chatroom_channel
+    stream_from "chatroom_channel"
   end
 
   def unsubscribed
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   post 'message', to: 'messages#create'
+  # Establishes websocket connection
+  mount ActionCable.server, at: '/cable'
 end
