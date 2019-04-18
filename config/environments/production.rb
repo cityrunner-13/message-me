bc08e34393c7a30ca096a97c195b6fa7f12d7a16
Marc <akbar13@live.com>
2019-04-18 15:56:13 -0600

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,8 +24,9 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
+  config.serve_static_assets = true
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
@@ -71,7 +72,7 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
+  config.i18n.fallbacks = [I18n.default_locale]
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
